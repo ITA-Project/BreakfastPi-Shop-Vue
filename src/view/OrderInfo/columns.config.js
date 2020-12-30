a09f36b7f04a5ea66a006045d1b7c84ecbd59b48
Felix <742863722@qq.com>
2020-12-30 20:22:43 +0800

    adjust the width of table columns

--- src/view/OrderInfo/columns.config.js
+++ src/view/OrderInfo/columns.config.js
@@ -2,29 +2,29 @@ const columnSlots = ['orderNumber', 'status', 'amount', 'createTime', 'updateTim
 const innerColumnSlots = ['inner-name', 'inner-description', 'inner-price', 'inner-quantity']
 
 const outstandingColumns = [
-  {title: 'Order Number', dataIndex: 'orderNumber', key: 'orderNumber', scopedSlots: {customRender: 'orderNumber'}},
-  {title: 'Status', dataIndex: 'status', key: 'status', scopedSlots: {customRender: 'status'}},
-  {title: 'Amount', dataIndex: 'amount', key: 'amount', scopedSlots: {customRender: 'amount'}},
-  {title: 'Created Time', dataIndex: 'createTime', key: 'createTime', scopedSlots: {customRender: 'dateTime'}},
-  {title: 'Updated Time', dataIndex: 'updateTime', key: 'updateTime', scopedSlots: {customRender: 'dateTime'}},
-  {title: 'Action', key: 'action', scopedSlots: {customRender: 'action'}}
+  {title: 'Order Number', width: '20%', ellipsis: 'true', dataIndex: 'orderNumber', key: 'orderNumber', scopedSlots: {customRender: 'orderNumber'}},
+  {title: 'Status', width: '15%', ellipsis: 'true', dataIndex: 'status', key: 'status', scopedSlots: {customRender: 'status'}},
+  {title: 'Amount', width: '15%', ellipsis: 'true', dataIndex: 'amount', key: 'amount', scopedSlots: {customRender: 'amount'}},
+  {title: 'Created Time', width: '15%', ellipsis: 'true', dataIndex: 'createTime', key: 'createTime', scopedSlots: {customRender: 'dateTime'}},
+  {title: 'Updated Time', width: '15%', ellipsis: 'true', dataIndex: 'updateTime', key: 'updateTime', scopedSlots: {customRender: 'dateTime'}},
+  {title: 'Action', width: '15%', key: 'action', scopedSlots: {customRender: 'action'}}
 ]
 
 const allColumns = [
-  {title: 'Order Number', dataIndex: 'orderNumber', key: 'orderNumber', scopedSlots: {customRender: 'orderNumber'}},
-  {title: 'Status', dataIndex: 'status', key: 'status', scopedSlots: {customRender: 'status'}},
-  {title: 'Amount', dataIndex: 'amount', key: 'amount', scopedSlots: {customRender: 'amount'}},
-  {title: 'Created Time', dataIndex: 'createTime', key: 'createTime', scopedSlots: {customRender: 'dateTime'}},
-  {title: 'Cancel Time', dataIndex: 'cancelTime', key: 'cancelTime', scopedSlots: {customRender: 'dateTime'}},
-  {title: 'Deliver Time', dataIndex: 'deliverTime', key: 'deliverTime', scopedSlots: {customRender: 'dateTime'}},
-  {title: 'Completed Time', dataIndex: 'completedTime', key: 'completedTime', scopedSlots: {customRender: 'dateTime'}}
+  {title: 'Order Number', width: '20%', ellipsis: 'true', dataIndex: 'orderNumber', key: 'orderNumber', scopedSlots: {customRender: 'orderNumber'}},
+  {title: 'Status', width: '10%', ellipsis: 'true', dataIndex: 'status', key: 'status', scopedSlots: {customRender: 'status'}},
+  {title: 'Amount', width: '10%', ellipsis: 'true', dataIndex: 'amount', key: 'amount', scopedSlots: {customRender: 'amount'}},
+  {title: 'Created Time', width: '15%', ellipsis: 'true', dataIndex: 'createTime', key: 'createTime', scopedSlots: {customRender: 'dateTime'}},
+  {title: 'Cancel Time', width: '15%', ellipsis: 'true', dataIndex: 'cancelTime', key: 'cancelTime', scopedSlots: {customRender: 'dateTime'}},
+  {title: 'Deliver Time', width: '15%', ellipsis: 'true', dataIndex: 'deliverTime', key: 'deliverTime', scopedSlots: {customRender: 'dateTime'}},
+  {title: 'Completed Time', width: '15%', ellipsis: 'true', dataIndex: 'completedTime', key: 'completedTime', scopedSlots: {customRender: 'dateTime'}}
 ]
 
 const innerColumns = [
-  {title: 'Name', dataIndex: 'product.name', key: 'name', scopedSlots: {customRender: 'inner-name'}},
-  {title: 'Description', dataIndex: 'product.description', key: 'description', scopedSlots: {customRender: 'inner-descirption'}},
-  {title: 'Price', dataIndex: 'product.price', key: 'price', scopedSlots: {customRender: 'inner-price'}},
-  {title: 'Quantity', dataIndex: 'quantity', key: 'quantity', scopedSlots: {customRender: 'inner-quantity'}}
+  {title: 'Name', width: '25%', ellipsis: 'true', dataIndex: 'product.name', key: 'name', scopedSlots: {customRender: 'inner-name'}},
+  {title: 'Description', width: '25%', ellipsis: 'true', dataIndex: 'product.description', key: 'description', scopedSlots: {customRender: 'inner-descirption'}},
+  {title: 'Price', width: '25%', ellipsis: 'true', dataIndex: 'product.price', key: 'price', scopedSlots: {customRender: 'inner-price'}},
+  {title: 'Quantity', width: '25%', ellipsis: 'true', dataIndex: 'quantity', key: 'quantity', scopedSlots: {customRender: 'inner-quantity'}}
 ]
 
 export default {
--- src/view/ProductManagement/columns.config.js
+++ src/view/ProductManagement/columns.config.js
@@ -3,24 +3,24 @@ const innerColumnSlots = ['inner-name', 'inner-description', 'inner-image', 'inn
   'inner-sales', 'inner-status', 'inner-createTime', 'inner-updateTime', 'inner-action']
 
 const columns = [
-  {title: 'Sequence', dataIndex: 'sequence', key: 'sequence', scopedSlots: {customRender: 'sequence'}},
-  {title: 'Name', dataIndex: 'name', key: 'name', scopedSlots: {customRender: 'name'}},
-  {title: 'Status', dataIndex: 'status', key: 'status', scopedSlots: {customRender: 'status'}},
-  {title: 'Created Time', dataIndex: 'createTime', key: 'createTime', scopedSlots: {customRender: 'dateTime'}},
-  {title: 'Updated Time', dataIndex: 'updateTime', key: 'updateTime', scopedSlots: {customRender: 'dateTime'}},
-  {title: 'Action', key: 'action', scopedSlots: {customRender: 'action'}}
+  {title: 'Sequence', width: '10%', dataIndex: 'sequence', key: 'sequence', scopedSlots: {customRender: 'sequence'}},
+  {title: 'Name', width: '20%', ellipsis: 'true', dataIndex: 'name', key: 'name', scopedSlots: {customRender: 'name'}},
+  {title: 'Status', width: '10%', ellipsis: 'true', dataIndex: 'status', key: 'status', scopedSlots: {customRender: 'status'}},
+  {title: 'Created Time', width: '20%', ellipsis: 'true', dataIndex: 'createTime', key: 'createTime', scopedSlots: {customRender: 'dateTime'}},
+  {title: 'Updated Time', width: '20%', ellipsis: 'true', dataIndex: 'updateTime', key: 'updateTime', scopedSlots: {customRender: 'dateTime'}},
+  {title: 'Action', width: '20%', key: 'action', scopedSlots: {customRender: 'action'}}
 ]
 
 const innerColumns = [
-  {title: 'Name', dataIndex: 'name', key: 'name', scopedSlots: {customRender: 'inner-name'}},
-  {title: 'Description', dataIndex: 'description', key: 'description', scopedSlots: {customRender: 'inner-descirption'}},
-  {title: 'Price', dataIndex: 'price', key: 'price', scopedSlots: {customRender: 'inner-price'}},
-  {title: 'Stock', dataIndex: 'stock', key: 'stock', scopedSlots: {customRender: 'inner-stock'}},
-  {title: 'Sales', dataIndex: 'sales', key: 'sales', scopedSlots: {customRender: 'inner-sales'}},
-  {title: 'Status', dataIndex: 'status', key: 'states', scopedSlots: {customRender: 'inner-status'}},
-  {title: 'Created Time', dataIndex: 'createTime', key: 'createTime', scopedSlots: {customRender: 'dateTime'}},
-  {title: 'Updated Time', dataIndex: 'updateTime', key: 'updateTime', scopedSlots: {customRender: 'dateTime'}},
-  {title: 'Action', key: 'action', scopedSlots: {customRender: 'inner-action'}
+  {title: 'Name', width: '10%', ellipsis: 'true', dataIndex: 'name', key: 'name', scopedSlots: {customRender: 'inner-name'}},
+  {title: 'Description', width: '20%', ellipsis: 'true', dataIndex: 'description', key: 'description', scopedSlots: {customRender: 'inner-descirption'}},
+  {title: 'Price', width: '7%', ellipsis: 'true', dataIndex: 'price', key: 'price', scopedSlots: {customRender: 'inner-price'}},
+  {title: 'Stock', width: '7%', ellipsis: 'true', dataIndex: 'stock', key: 'stock', scopedSlots: {customRender: 'inner-stock'}},
+  {title: 'Sales', width: '7%', ellipsis: 'true', dataIndex: 'sales', key: 'sales', scopedSlots: {customRender: 'inner-sales'}},
+  {title: 'Status', width: '9%', ellipsis: 'true', dataIndex: 'status', key: 'states', scopedSlots: {customRender: 'inner-status'}},
+  {title: 'Created Time', width: '15%', ellipsis: 'true', dataIndex: 'createTime', key: 'createTime', scopedSlots: {customRender: 'dateTime'}},
+  {title: 'Updated Time', width: '15%', ellipsis: 'true', dataIndex: 'updateTime', key: 'updateTime', scopedSlots: {customRender: 'dateTime'}},
+  {title: 'Action', width: '10%', key: 'action', scopedSlots: {customRender: 'inner-action'}
   }
 ]
 
